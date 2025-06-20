2217569a3115d519a3a846f3bfe7bce4e9b78860
rigili27 <rigili27@gmail.com>
2025-06-20 06:02:06 -0300

    comment function

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -17,27 +17,27 @@ class Business extends Model
 
     protected static function booted()
     {
-        // Eliminar imágenes cuando se actualiza el modelo y cambian las imágenes
-        static::updating(function (Business $business) {
-            $original = $business->getOriginal(); // Obtener los valores antes de la actualización
+        // // Eliminar imágenes cuando se actualiza el modelo y cambian las imágenes
+        // static::updating(function (Business $business) {
+        //     $original = $business->getOriginal(); // Obtener los valores antes de la actualización
 
-            if ($original['desktop_logo_url'] !== $business->desktop_logo_url) {
-                Log::info("Updating - Eliminando imagen antigua: public/{$original['desktop_logo_url']}");
-                Storage::disk('public')->delete($original['desktop_logo_url']);
-            }
+        //     if ($original['desktop_logo_url'] !== $business->desktop_logo_url) {
+        //         Log::info("Updating - Eliminando imagen antigua: public/{$original['desktop_logo_url']}");
+        //         Storage::disk('public')->delete($original['desktop_logo_url']);
+        //     }
 
-            if ($original['mobile_logo_url'] !== $business->mobile_logo_url) {
-                Log::info("Updating - Eliminando imagen antigua: public/{$original['mobile_logo_url']}");
-                Storage::disk('public')->delete($original['mobile_logo_url']);
-            }
-        });
+        //     if ($original['mobile_logo_url'] !== $business->mobile_logo_url) {
+        //         Log::info("Updating - Eliminando imagen antigua: public/{$original['mobile_logo_url']}");
+        //         Storage::disk('public')->delete($original['mobile_logo_url']);
+        //     }
+        // });
 
-        // Eliminar imágenes cuando se borra el modelo
-        static::deleted(function (Business $business) {
-            Log::info("Deleted - Eliminando imágenes de: public/{$business->desktop_logo_url} y {$business->mobile_logo_url}");
+        // // Eliminar imágenes cuando se borra el modelo
+        // static::deleted(function (Business $business) {
+        //     Log::info("Deleted - Eliminando imágenes de: public/{$business->desktop_logo_url} y {$business->mobile_logo_url}");
 
-            Storage::disk('public')->delete($business->desktop_logo_url);
-            Storage::disk('public')->delete($business->mobile_logo_url);
-        });
+        //     Storage::disk('public')->delete($business->desktop_logo_url);
+        //     Storage::disk('public')->delete($business->mobile_logo_url);
+        // });
     }
 }
